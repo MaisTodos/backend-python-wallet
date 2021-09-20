59031f2bdea0a3958f1902c7f4334503916bd528
Rodrigo Xavier <rodrigomatiasxavier@gmail.com>
2021-09-20 02:07:07 -0300

    Alterando as urls de sales

--- sales/urls.py
+++ sales/urls.py
@@ -3,3 +3,12 @@ from rest_framework import routers
 from django.conf.urls import include
 from sales.api import viewsets
 from rest_framework.authtoken import views
+
+app_name = 'sales'
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('api/', include(router.urls), name='api'),
+    path('api-token-auth/', views.obtain_auth_token),
+]
