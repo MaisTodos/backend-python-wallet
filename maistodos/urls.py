c3fccf691d844833ab12fb20dc8652af454b6ab1
Rodrigo Xavier <rodrigomatiasxavier@gmail.com>
2021-09-20 02:06:23 -0300

    Alterando as urls de samplemed

--- maistodos/urls.py
+++ maistodos/urls.py
@@ -11,11 +11,26 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('sales/', include('sales.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+    
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('sales.urls', namespace="sales")),
+    path('api-auth/', include('rest_framework.urls')),
+    path('admin/', admin.site.urls, name="admin"),
+    path('admin/', RedirectView.as_view(url='/admin'), name='super-user')
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
