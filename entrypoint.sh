d6858bb54c49babdb1f00d7b67515d82ca59e034
Rodrigo Xavier <rodrigomatiasxavier@gmail.com>
2021-09-20 01:46:37 -0300

    Adicionando entrypoint para execuçao automatica de comandos shell no start do container django

---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install new requirements
+echo "INSTALL NEW REQUIREMENTS"
+pip install -r requirements.txt
+
+# Collect static files
+echo "COLLECT STATIC FILES"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "MAKING MIGRATIONS"
+python manage.py makemigrations
+
+echo "APPLY DATABASE MIGRATIONS"
+python manage.py migrate
+
+# if [ ${DEBUG} == True ]
+# then
+#     # Running Tests
+#     echo "RUNING TESTS"
+#     pytest
+#     #TODO : stop database test after tests
+# fi
+
+# Start server
+echo "STARTING SERVER"
+python manage.py runserver 0.0.0.0:8000
